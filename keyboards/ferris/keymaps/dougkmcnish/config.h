7851dd9e32ed8ab92434d1c81e309831b99c2f96
Doug McNish <doug@mcnishes.com>
2022-10-13 08:46:08 -0400

    keymap changes and key chords.

--- keyboards/ferris/keymaps/dougkmcnish/config.h
+++ keyboards/ferris/keymaps/dougkmcnish/config.h
@@ -18,5 +18,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 // Pick good defaults for enabling homerow modifiers
-#define TAPPING_TERM 200
+#define TAPPING_TERM 180
 #define IGNORE_MOD_TAP_INTERRUPT
+#define COMBO_VARIABLE_LEN
+#define COMBO_TERM 50
+#define ONESHOT_TAP_TOGGLE 5  /* Tapping this number of times holds the key until tapped once again. */
+#define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
--- keyboards/ferris/keymaps/dougkmcnish/rules.mk
+++ keyboards/ferris/keymaps/dougkmcnish/rules.mk
@@ -1,2 +1,4 @@
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 TAP_DANCE_ENABLE = yes
+VPATH += keyboards/gboards/
+COMBO_ENABLE=yes
